81512391d4ad03ca92d56fc905174134be700b6f
Miracle <735143929@qq.com>
2026-03-09 17:52:28 +0800

    Initial commit

---
+++ src/main/java/com/mapp/db/gen/entity/TableInfo.java
@@ -0,0 +1,18 @@
+package com.mapp.db.gen.entity;
+
+import lombok.Data;
+
+import java.util.*;
+
+@Data
+public class TableInfo {
+
+    private int   tableIndex;
+    private String tableName;
+    private String type;
+    private String tableComment;
+    private Date createTime;
+    private String author;
+    private Set<String> pkName = new HashSet<>();
+    private List<ColumnInfo> columnInfoList = new ArrayList<>();
+}
